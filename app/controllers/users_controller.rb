01b177c9c14dd858f1527c0e3eb8cfb6f6bd3297
charlesmferguson <charles.m.ferguson.jr@gmail.com>
2016-11-18 01:17:10 -0500

    got up to 5.11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
@@ -16,8 +17,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def update
